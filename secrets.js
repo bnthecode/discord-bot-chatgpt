082d4774e76530c82fc4c6ceaa8f1d28564b0bb6
bnthecode <brandon.counts.personal@gmail.com>
2023-02-04 03:23:31 -0500

    um i cant read

--- secrets.js
+++ secrets.js
@@ -2,9 +2,7 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 const environment = {
-  open_ai_secret:
-    process.env.OPENAI_API_KEY ||
-    "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",
+  open_ai_secret: process.env.OPENAI_API_KEY || "",
   discord_secret: process.env.DISCORD_SECRET || "",
   // channel ID's that bot is allowed to respond to
   bot_channel_ids: [process.env.BOT_CHANNEL_ID || "", "975715018421178368"],
